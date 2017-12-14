aecc17f7fb8e3919cf654465c734aca8425cb67c
Cristophor Hurduban <34269015+cristophor22@users.noreply.github.com>
2017-12-14 11:18:23 +0200

    added plugin files

---
+++ src/Containers/PlaceholderContainer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Placeholder\Containers;
+
+use Plenty\Plugin\Templates\Twig;
+
+class PlaceholderContainer
+{
+
+    public function call( Twig $twig ) : string
+    {
+
+        return $twig->render( 'Placeholder::content.Placeholder' );
+
+    }
+
+}
